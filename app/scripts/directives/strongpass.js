5864b962f20bb7bf21017c6898e303771b26b16e
Sebastian Vidal Aedo <sebastianvidalaedo@gmail.com>
2017-02-14 16:52:53 -0300

    mostrar mensajes al quitar el foco y limpiarlos al hacer foco en la contraseña

--- app/scripts/directives/strongpass.js
+++ app/scripts/directives/strongpass.js
@@ -7,18 +7,28 @@ app.directive('strongPass', function () {
         require: 'ngModel',
         restrict: 'A',
         link: function (scope, element, attributes, ngModel) {
-            var regex = /^(?:(?=.*?[A-Z])(?:(?=.*?[0-9])(?=.*?[-!@#$%^&*()_[\]{},.<>+=])|(?=.*?[a-z])(?:(?=.*?[0-9])|(?=.*?[-!@#$%^&*()_[\]{},.<>+=])))|(?=.*?[a-z])(?=.*?[0-9])(?=.*?[-!@#$%^&*()_[\]{},.<>+=]))[A-Za-z0-9!@#$%^&*()_[\]{},.<>+=-]{8,50}$/;
+            element.on('focus', function () {
+                ngModel.$setValidity('invalidchar', true);
+                ngModel.$setValidity('strongPassword', true);
+                scope.$apply();
+            });
 
-            ngModel.$validators.strongPassword = function (value) {
-                return value !== undefined && regex.test(value);
-            };
-
-            element.on('input focus blur', function () {
+            element.on('blur', function () {
                 var validchars = /^[a-zA-Z0-9-!@#$%^&*()_[\]{},.<>+=]+$/;
                 var value = ngModel.$viewValue;
 
                 ngModel.$setValidity('invalidchar', value.length && validchars.test(value));
                 scope.$apply();
+
+                if( value.length && validchars.test(value) ){
+                    var regex = /^(?:(?=.*?[A-Z])(?:(?=.*?[0-9])(?=.*?[-!@#$%^&*()_[\]{},.<>+=])|(?=.*?[a-z])(?:(?=.*?[0-9])|(?=.*?[-!@#$%^&*()_[\]{},.<>+=])))|(?=.*?[a-z])(?=.*?[0-9])(?=.*?[-!@#$%^&*()_[\]{},.<>+=]))[A-Za-z0-9!@#$%^&*()_[\]{},.<>+=-]{8,50}$/;
+                    var valid = false;
+                    if( regex.test(value) ){
+                        valid = true;
+                    }
+                    ngModel.$setValidity('strongPassword', valid);
+                    scope.$apply();
+                }
             });
         }
     }
