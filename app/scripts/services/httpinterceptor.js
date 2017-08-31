c24ad00227570bf9d555923d9cadc88b4ee5e1c1
Sebastian Vidal Aedo <sebastianvidalaedo@gmail.com>
2017-08-31 10:28:47 -0300

    fix listado de tramites para usuario autenticado

--- app/scripts/services/httpinterceptor.js
+++ app/scripts/services/httpinterceptor.js
@@ -4,22 +4,22 @@ function HttpInterceptor($cookies) {
     'ngInject';
 
     this.request = function (config) {
-        // if (config.url.indexOf('claveunica') !== - 1) {
+        console.log("=>"+config.url+"<=");
+        if (config.url.indexOf('https://apis.digital.gob.cl/misc/instituciones') > -1) {
+            return config;
+        } else {
             config.withCredentials = true;
-            // config.headers['X-CSRFToken'] = $cookies.get('csrftoken');
-            // config.headers['X-Xsrftoken'] = $cookies.get('_xsrf');
-        // }
-        if( localStorage.getItem('token') != null ){
-          config.headers['token'] = localStorage.getItem('token');
+            if( localStorage.getItem('token') != null ){
+              config.headers['token'] = localStorage.getItem('token');
+            }
+            if( localStorage.getItem('session') != null ){
+              config.headers['session'] = localStorage.getItem('session');
+            }
+            return config;
         }
-        if( localStorage.getItem('session') != null ){
-          config.headers['session'] = localStorage.getItem('session');
-        }
-
-        return config;
     };
 }
-
+ 
 angular
     .module('claveunica')
     .service('HttpInterceptor', HttpInterceptor)
